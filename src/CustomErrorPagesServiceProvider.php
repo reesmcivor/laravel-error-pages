e5a65c04f9a2060b32be499055072364caa69f06
reesmcivor <reesmcivor@hotmail.com>
2023-05-02 10:53:07 +0100

    auto

--- src/CustomErrorPagesServiceProvider.php
+++ src/CustomErrorPagesServiceProvider.php
@@ -10,7 +10,9 @@ class CustomErrorPagesServiceProvider extends ServiceProvider
     {
         $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
 
-        $this->loadViewsFrom(__DIR__ . '/views', 'custom_error_page');
+        $this->loadViewsFrom(__DIR__ . '/views', 'laravel_error_pages');
+
+        $this->mergeConfigFrom(__DIR__.'/config/laravel_error_pages.php', 'laravel_error_pages');
 
         $this->publishes([
             __DIR__ . '/views' => resource_path('views'),
