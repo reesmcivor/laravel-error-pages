ffed0f05934370c337df4ad4c56b5362cfedbf57
reesmcivor <reesmcivor@hotmail.com>
2023-05-02 10:32:23 +0100

    auto

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,5 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/test-500-error', function () {
+    config(['app.debug' => false]);
     throw new \Exception('This is a test 500 error.');
 });
